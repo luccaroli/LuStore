e7bb0c0f98c8d319011c38e89d6e8e812756770a
luccaroli <luccaroli20@gmail.com>
2020-07-15 12:47:31 -0300

    apresentando dados do backend no frontend page show

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,11 +6,15 @@ module.exports = {
     const year = date.getUTCFullYear()
     const month = `0${date.getUTCMonth() + 1}`.slice(-2)
     const day = `0${date.getUTCDate()}`.slice(-2)
+    const hour = date.getUTCHours()
+    const minutes = date.getUTCMinutes()
 
     return {
       day, 
       month,
       year,
+      hour,
+      minutes,
       iso: `${year}-${month}-${day}`,
       birthDay: `${day}/${month}`,
       format: `${day}/${month}/${year}`
